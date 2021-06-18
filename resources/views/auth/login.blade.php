efcaff2cb4a1f49c12ec43081944b22b24eaa4ef
mejatojo <mejaramanandraibe04@gmail.com>
2021-06-18 07:55:34 +0200

    trustproxies

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,11 +33,11 @@
                         @csrf
         <div class="form-group has-feedback">
           <input type="email" class="form-control" placeholder="Email" name="email">
-          <span class="fa fa-envelope form-control-feedback"></span>
+          <!-- <span class="fa fa-envelope form-control-feedback"></span> -->
         </div>
         <div class="form-group has-feedback">
           <input type="password" class="form-control" placeholder="Password" name="password">
-          <span class="fa fa-lock form-control-feedback"></span>
+          <!-- <span class="fa fa-lock form-control-feedback"></span> -->
         </div>
         <div class="row">
           <div class="col-8">
